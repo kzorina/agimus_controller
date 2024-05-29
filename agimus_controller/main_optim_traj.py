7d285ec583fe2c0a2de2a26f739a1f87ecf4f26b
Arthur <arthur.haffemayer@yahoo.fr>
2024-05-29 13:35:39 +0000

    renaming main into main optim traj

--- agimus_controller/main_optim_traj.py
+++ agimus_controller/main_optim_traj.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import pinocchio as pin
 
-# from wrapper_meshcat import MeshcatWrapper
+from wrapper_meshcat import MeshcatWrapper
 from wrapper_panda import PandaWrapper
 from ocp import OCPPandaReachingColWithMultipleCol
 from scenes import Scene
@@ -19,16 +19,16 @@ rmodel, cmodel, vmodel = robot_wrapper()
 
 # Creating the scene
 scene = Scene()
-cmodel, TARGET, q0 = scene.create_scene(rmodel, cmodel, "box")
+cmodel, TARGET, q0 = scene.create_scene(rmodel, cmodel, "ball")
 
-# # Generating the meshcat visualizer
-# MeshcatVis = MeshcatWrapper()
-# vis, meshcatVis = MeshcatVis.visualize(
-#     TARGET,
-#     robot_model=rmodel,
-#     robot_collision_model=cmodel,
-#     robot_visual_model=vmodel,
-# )
+# Generating the meshcat visualizer
+MeshcatVis = MeshcatWrapper()
+vis, meshcatVis = MeshcatVis.visualize(
+    TARGET,
+    robot_model=rmodel,
+    robot_collision_model=cmodel,
+    robot_visual_model=vmodel,
+)
 
 ### INITIAL X0
 x0 = np.concatenate([q0, pin.utils.zero(rmodel.nv)])
@@ -56,12 +56,12 @@ US_init = ddp.problem.quasiStatic(XS_init[:-1])
 ddp.solve(XS_init, US_init)
 
 print("End of the computation, press enter to display the traj if requested.")
-# ### DISPLAYING THE TRAJ
-# while True:
-#     vis.display(q0)
-#     input()
-#     for xs in ddp.xs:
-#         vis.display(np.array(xs[:7].tolist()))
-#         time.sleep(1e-1)
-#     input()
-#     print("replay")
+### DISPLAYING THE TRAJ
+while True:
+    vis.display(q0)
+    input()
+    for xs in ddp.xs:
+        vis.display(np.array(xs[:7].tolist()))
+        time.sleep(1e-1)
+    input()
+    print("replay")
