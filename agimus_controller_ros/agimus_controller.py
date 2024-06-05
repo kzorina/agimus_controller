d76f7e1ab6e720db1609da3524fb92bc2fb0d7b4
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-06-05 11:02:44 +0000

    Continue adding the subscriber

--- agimus_controller_ros/agimus_controller.py
+++ agimus_controller_ros/agimus_controller.py
@@ -4,6 +4,8 @@ from geometry_msgs.msg import Vector3
 
 class AgimusControllerNode:
     def __init__(self) -> None:
+        self.op_frames = rospy.get_param("op_frames", [])
+
         self.subscribers = []
         self.subscribers += [
             rospy.Subscriber("com/position", Vector3, self.com_position_callback)
@@ -11,12 +13,20 @@ class AgimusControllerNode:
         self.subscribers += [
             rospy.Subscriber("com/velocity", Vector3, self.com_velocity_callback)
         ]
+        for op_frame in self.op_frames:
+            self.subscribers += [
+                rospy.Subscriber("com/velocity", Vector3, self.com_velocity_callback)
+            ]
 
     def run(self):
         pass
 
     def com_position_callback(self, msg):
-        pass
+        print("CoM pos msg = ", msg)
 
     def com_velocity_callback(self, msg):
         print("CoM vel msg = ", msg)
+
+    def op_frames_callback(self, op_frame, msg):
+        print("Op frame = ", msg)
+        print(op_frame)
