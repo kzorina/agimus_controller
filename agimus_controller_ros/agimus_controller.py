c3b1dbc177d1079b37a76676c1319dad462bf38c
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-06-05 09:45:32 +0000

    Start adding the subscribers

--- agimus_controller_ros/agimus_controller.py
+++ agimus_controller_ros/agimus_controller.py
@@ -1,9 +1,20 @@
 import rospy
+from geometry_msgs.msg import Vector3, Transform
+from dynamic_graph_bridge_msgs.msg import Vector
 
 class AgimusControllerNode:
 
     def __init__(self) -> None:
-        pass
+        
+        self.subscribers = []
+        self.subscribers += [rospy.Subscriber("com/position", Vector3, self.com_position_callback)]
+        self.subscribers += [rospy.Subscriber("com/velocity", Vector3, self.com_velocity_callback)]
 
     def run(self):
         pass
+
+    def com_position_callback(self, msg):
+        pass
+
+    def com_velocity_callback(self, msg):
+        print("CoM vel msg = ", msg)
