9e345f81cd24d645d90a5108f73e66e300b6202e
Arthur <arthur.haffemayer@yahoo.fr>
2024-06-18 12:51:47 +0000

    getting the robot creation out of the mpc

--- agimus_controller/mpc.py
+++ agimus_controller/mpc.py
@@ -3,6 +3,8 @@ from .ocp_croco_hpp import OCPCrocoHPP
 
 
 class TrajectoryBuffer:
+    """List of variable size in which the HPP trajectory nodes will be.
+    """
     def __init__(self, model):
         self.model = model
         self.x_plan = []
@@ -36,12 +38,24 @@ class TrajectoryBuffer:
 
 
 class MPC:
-    def __init__(self, x_plan, a_plan, robot_name):
-        self.prob = OCPCrocoHPP(robot_name)
+    """Create the MPC problem
+    """
+    def __init__(self, ocp, x_plan:np.ndarray, a_plan:np.ndarray, rmodel, cmodel):
+        """Initiate the MPC problem.
+
+        Args:
+            ocp (OCP class): OCP describing the problem.
+            x_plan (np.ndarray): State planification of HPP.
+            a_plan (np.ndarray): Acceleration planification of HPP.
+            rmodel (pin.Model): Pinocchio model of the robot
+            cmodel (pin.CollisionModel): Pinocchio collision model.w
+        """
+        self._ocp = ocp
         self.whole_x_plan = x_plan
         self.whole_a_plan = a_plan
-        self.robot = self.prob.robot
-        self.nq = self.robot.nq
+        self.rmodel = rmodel
+        self.cmodel = cmodel
+        self.nq = self.rmodel.nq
         self.croco_xs = None
         self.croco_us = None
         self.whole_traj_T = x_plan.shape[0]
@@ -50,12 +64,12 @@ class MPC:
         """Get state at the next step by doing a crocoddyl integration."""
         m = problem.runningModels[0]
         d = m.createData()
-        m.calc(d, x, self.prob.solver.us[0])
+        m.calc(d, x, self._ocp.solver.us[0])
         return d.xnext.copy()
 
     def simulate_mpc(self, T, use_constraints=False, node_idx_breakpoint=None):
         """Simulate mpc behavior using crocoddyl integration as a simulator."""
-        self.prob.use_constraints = use_constraints
+        self._ocp.use_constraints = use_constraints
         mpc_xs = np.zeros([self.whole_traj_T, 2 * self.nq])
         mpc_us = np.zeros([self.whole_traj_T - 1, self.nq])
         x0 = self.whole_x_plan[0, :]
@@ -90,23 +104,23 @@ class MPC:
 
     def mpc_first_step(self, x_plan, a_plan, x0, T):
         """Create crocoddyl problem from planning, run solver and get new state."""
-        problem = self.prob.build_ocp_from_plannif(x_plan, a_plan, x0)
-        self.prob.run_solver(
-            problem, list(x_plan), list(self.prob.u_ref[: T - 1]), 1000
+        problem = self._ocp.build_ocp_from_plannif(x_plan, a_plan, x0)
+        self._ocp.run_solver(
+            problem, list(x_plan), list(self._ocp.u_ref[: T - 1]), 1000
         )
-        x = self.get_next_state(x0, self.prob.solver.problem)
-        return x, self.prob.solver.us[0]
+        x = self.get_next_state(x0, self._ocp.solver.problem)
+        return x, self._ocp.solver.us[0]
 
     def mpc_step(self, x, x_plan, a_plan):
         """Reset ocp, run solver and get new state."""
-        u_ref_terminal_node = self.prob.get_inverse_dynamic_control(
+        u_ref_terminal_node = self._ocp.get_inverse_dynamic_control(
             x_plan[-1], a_plan[-1]
         )
-        self.prob.reset_ocp(x, x_plan[-1], u_ref_terminal_node[: self.nq])
-        xs_init = list(self.prob.solver.xs[1:]) + [self.prob.solver.xs[-1]]
+        self._ocp.reset_ocp(x, x_plan[-1], u_ref_terminal_node[: self.nq])
+        xs_init = list(self._ocp.solver.xs[1:]) + [self._ocp.solver.xs[-1]]
         xs_init[0] = x
-        us_init = list(self.prob.solver.us[1:]) + [self.prob.solver.us[-1]]
-        self.prob.solver.problem.x0 = x
-        self.prob.run_solver(self.prob.solver.problem, xs_init, us_init, 1)
-        x = self.get_next_state(x, self.prob.solver.problem)
-        return x, self.prob.solver.us[0]
+        us_init = list(self._ocp.solver.us[1:]) + [self._ocp.solver.us[-1]]
+        self._ocp.solver.problem.x0 = x
+        self._ocp.run_solver(self._ocp.solver.problem, xs_init, us_init, 1)
+        x = self.get_next_state(x, self._ocp.solver.problem)
+        return x, self._ocp.solver.us[0]
