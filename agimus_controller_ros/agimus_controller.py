b6a7cff14810c833b84bfac34e38aea5a80b2631
Théo Martinez <tmartinezf@laas.fr>
2024-08-07 11:16:19 +0200

    remove run function in agimus_controller.py

--- agimus_controller_ros/agimus_controller.py
+++ agimus_controller_ros/agimus_controller.py
@@ -12,18 +12,3 @@ class AgimusControllerNode(ControllerBase):
 
     def get_next_trajectory_point(self):
         return self.hpp_subscriber.get_trajectory_point()
-
-    def run(self):
-        self.wait_first_sensor_msg()
-        self.wait_buffer_has_twice_horizon_points()
-        sensor_msg, u, k = self.first_solve()
-        input("Press enter to continue ...")
-        self.send(sensor_msg, u, k)
-        self.rate.sleep()
-        while not rospy.is_shutdown():
-            start_compute_time = rospy.Time.now()
-            sensor_msg, u, k = self.solve()
-            self.send(sensor_msg, u, k)
-            self.rate.sleep()
-            self.ocp_solve_time.data = rospy.Time.now() - start_compute_time
-            self.ocp_solve_time_pub.publish(self.ocp_solve_time)
