0e67017e3cefae757f1ac6da061d20a0f07a641e
Théo Martinez <tmartinezf@laas.fr>
2024-10-21 08:26:50 +0000

    move mpc parameters yaml

--- agimus_controller/utils/path_finder.py
+++ agimus_controller/utils/path_finder.py
@@ -9,7 +9,9 @@ def get_package_path(package_name) -> Path:
 
 
 def get_mpc_params_dict():
-    agimus_controller_dir = get_package_path("agimus_controller")
-    mpc_params_file_path = agimus_controller_dir / "config" / "mpc_params.yaml"
+    agimus_demos_description_dir = get_package_path("agimus_demos_description")
+    mpc_params_file_path = (
+        agimus_demos_description_dir / "pick_and_place" / "mpc_params.yaml"
+    )
     with open(str(mpc_params_file_path), "r") as file:
         return yaml.safe_load(file)
