637222c786336454168b42218fc6d6f83f75bc4d
Maximilien Naveau <maximilien.naveau@gmail.com>
2024-12-09 14:50:18 +0100

    Add update_previous_solution into the warm_start

--- agimus_controller/agimus_controller/warm_start_base.py
+++ agimus_controller/agimus_controller/warm_start_base.py
@@ -7,12 +7,18 @@ from agimus_controller.trajectory import TrajectoryPoint
 class WarmStartBase(ABC):
     def __init__(self) -> None:
         super().__init__()
+        self._previous_solution: list[TrajectoryPoint] = list()
 
     @abstractmethod
     def generate(
         self,
         reference_trajectory: list[TrajectoryPoint],
-        previous_solution: list[TrajectoryPoint],
     ) -> tuple(np.ndarray, np.ndarray):
         """Returns x_init, u_init."""
         ...
+
+    def update_previous_solution(
+        self, previous_solution: list[TrajectoryPoint]
+    ) -> None:
+        """Stores internally the previous solution of the OCP"""
+        self._previous_solution = previous_solution
