0820ea7ff8f4cd52e00b1c33d43a6c289af821d1
Arthur Haffemayer <arthur.haffemayer@yahoo.fr>
2024-12-11 17:18:36 +0000

    add test for TestOcpBase

---
+++ agimus_controller/tests/test_ocp_base.py
@@ -0,0 +1,9 @@
+import unittest
+
+from agimus_controller.ocp_base import OCPBase
+
+
+class TestOCPBase(unittest.TestCase):
+    def test_abstract_class_instantiation(self):
+        with self.assertRaises(TypeError):
+            OCPBase()
