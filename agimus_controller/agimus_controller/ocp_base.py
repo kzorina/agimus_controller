eab8f12f9fe15da9b5e2d70f15836c8b62c5fbdf
Arthur Haffemayer <arthur.haffemayer@yahoo.fr>
2024-12-13 11:18:45 +0000

    docstring + bringing back x0 in the solve + changing the name x_init to x_warmstart for clarity's sake

--- agimus_controller/agimus_controller/ocp_base.py
+++ agimus_controller/agimus_controller/ocp_base.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import List
 
 import numpy as np
 import numpy.typing as npt
@@ -9,6 +8,10 @@ from agimus_controller.trajectory import WeightedTrajectoryPoint
 
 
 class OCPBase(ABC):
+    """Base class for the Optimal Control Problem (OCP) solver. This class defines the interface for the OCP solver.
+    If you want to implement a new OCP solver, you should derive from this class and implement the abstract methods.
+    If you want to use Crocoddyl, you should inherit from the OCPCrocoBase class instead.
+    """
     def __init__(self) -> None:
         pass
 
@@ -16,32 +19,63 @@ class OCPBase(ABC):
     def set_reference_horizon(
         self, reference_trajectory: list[WeightedTrajectoryPoint]
     ) -> None:
+        """Set the reference trajectory and the weights of the costs for the OCP solver. This method should be implemented by the derived class."""
         ...
 
     @abstractmethod
     @property
     def horizon_size() -> int:
+        """Returns the horizon size of the OCP.
+
+        Returns:
+            int: size of the horizon.
+        """
         ...
 
     @abstractmethod
     @property
     def dt() -> int:
+        """Returns the time step of the OCP.
+
+        Returns:
+            int: time step of the OCP.
+        """
         ...
 
     @abstractmethod
     def solve(
         self,
-        x_init: list[npt.NDArray[np.float64]],
-        u_init: list[npt.NDArray[np.float64]],
+        x0: npt.NDArray[np.float64],
+        x_warmstart: list[npt.NDArray[np.float64]],
+        u_warmstart: list[npt.NDArray[np.float64]],
     ) -> None:
+        """Solver for the OCP. This method should be implemented by the derived class.
+        The method should solve the OCP for the given initial state and warmstart values.
+        
+        Args:
+            x0 (npt.NDArray[np.float64]): current state of the robot.
+            x_warmstart (list[npt.NDArray[np.float64]]): Warmstart values for the state. This doesn't include the current state.
+            u_warmstart (list[npt.NDArray[np.float64]]): Warmstart values for the control inputs. 
+        """
         ...
 
     @abstractmethod
     @property
     def ocp_results(self) -> OCPResults:
+        """Returns the results of the OCP solver.
+        The solve method should be called before calling this method.
+
+        Returns:
+            OCPResults: Class containing the results of the OCP solver.
+        """
         ...
 
     @abstractmethod
     @property
     def debug_data(self) -> OCPDebugData:
+        """Returns the debug data of the OCP solver.
+
+        Returns:
+            OCPDebugData: Class containing the debug data of the OCP solver.
+        """
         ...
