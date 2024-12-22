23f0da71af8ff11a2f694393f4890d69072fdc24
kzorina <kateryna.zorina@cvut.cz>
2024-12-22 17:37:22 +0100

    add docstring to warmstart

--- agimus_controller/agimus_controller/warm_start_reference.py
+++ agimus_controller/agimus_controller/warm_start_reference.py
@@ -9,6 +9,45 @@ from agimus_controller.trajectory import TrajectoryPoint
 
 
 class WarmStartReference(WarmStartBase):
+    """
+    A class for generating warmstart values for trajectory optimization problem.
+
+    This class uses a reference trajectory and the robot model to compute the initial state, 
+    state vectors, and control inputs.
+
+    Attributes:
+    _rmodel (pin.Model | None): The robot's Pinocchio model, used for forward dynamics computations.
+    _rdata (pin.Data | None): Data structure associated with the Pinocchio model.
+
+    Methods:
+        setup(rmodel: pin.Model) -> None:
+            Initializes the robot model and its associated data structure for later computations.
+        
+        generate(
+            initial_state: TrajectoryPoint,
+            reference_trajectory: list[TrajectoryPoint],
+        ) -> tuple[
+            npt.NDArray[np.float64],
+            list[npt.NDArray[np.float64]],
+            list[npt.NDArray[np.float64]],
+        ]:
+            Generates the initial state, reference state vectors, and control inputs for warmstart.
+
+            Parameters:
+                initial_state (TrajectoryPoint): The starting state of the robot, containing joint configuration 
+                    and velocity information.
+                reference_trajectory (list[TrajectoryPoint]): A sequence of desired trajectory points, each containing 
+                    joint configuration, velocity, and acceleration.
+
+            Returns:
+                tuple: A tuple containing:
+                    - x0 (npt.NDArray[np.float64]): The initial state vector `[q, v]` where `q` is the joint configuration 
+                    and `v` is the joint velocity.
+                    - x_init (list[npt.NDArray[np.float64]]): A list of state vectors `[q, v]` constructed from the 
+                    reference trajectory.
+                    - u_init (list[npt.NDArray[np.float64]]): A list of control inputs computed using inverse dynamics 
+                    (RNEA) based on the reference trajectory.
+    """
     def __init__(self) -> None:
         super().__init__()
         self._rmodel: pin.Model | None = None
