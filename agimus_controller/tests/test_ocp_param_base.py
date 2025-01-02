9047baa9fb9f15e30d2921c50e0b6fdd682438a1
Arthur Haffemayer <arthur.haffemayer@yahoo.fr>
2025-01-02 15:00:35 +0000

    unit testing the ocp param base dataclass

--- agimus_controller/tests/test_ocp_param_base.py
+++ agimus_controller/tests/test_ocp_param_base.py
@@ -1,61 +1,75 @@
 import unittest
 import numpy as np
 
-<<<<<<< Updated upstream
-from agimus_controller.agimus_controller.ocp_param_base import OCPParamsCrocoBase
-from agimus_controller.agimus_controller.trajectory import (
-    WeightedTrajectoryPoint,
-    TrajectoryPoint,
-    TrajectoryPointWeights,
-)
-
-=======
 from agimus_controller.ocp_param_base import OCPParamsCrocoBase
-from agimus_controller.trajectory import WeightedTrajectoryPoint, TrajectoryPoint, TrajectoryPointWeights
->>>>>>> Stashed changes
 
 class TestOCPParamsCrocoBase(unittest.TestCase):
+    """
+    TestOCPParamsCrocoBase is a unit test class for testing the OCPParamsCrocoBase class.
+
+    Methods:
+        __init__(methodName="runTest"): Initializes the test case instance.
+        test_initialization(): Tests the initialization of the OCPParamsCrocoBase class with various parameters.
+    """
     """Test the OCPParamsCrocoBase class."""
 
     def __init__(self, methodName="runTest"):
         super().__init__(methodName)
 
     def test_initialization(self):
+        """
+        Test the initialization of the OCPParamsCrocoBase class.
+
+        This test verifies that the parameters passed to the OCPParamsCrocoBase
+        constructor are correctly assigned to the instance attributes.
+
+        Test Parameters:
+        - dt (float): Time step for the OCP solver.
+        - T (int): Total number of time steps.
+        - solver_iters (int): Number of solver iterations.
+        - qp_iters (int): Number of QP iterations.
+        - termination_tolerance (float): Tolerance for termination criteria.
+        - eps_abs (float): Absolute tolerance for the solver.
+        - eps_rel (float): Relative tolerance for the solver.
+        - callbacks (bool): Flag to enable or disable callbacks.
+
+        Assertions:
+        - Asserts that the instance attribute `dt` is equal to the input `dt`.
+        - Asserts that the instance attribute `T` is equal to the input `T`.
+        - Asserts that the instance attribute `qp_iters` is equal to the input `qp_iters`.
+        - Asserts that the instance attribute `solver_iters` is equal to the input `solver_iters`.
+        - Asserts that the instance attribute `termination_tolerance` is equal to the input `termination_tolerance`.
+        - Asserts that the instance attribute `eps_abs` is equal to the input `eps_abs`.
+        - Asserts that the instance attribute `eps_rel` is equal to the input `eps_rel`.
+        - Asserts that the instance attribute `callbacks` is False.
+        """
         dt = np.float64(0.01)
         T = 100
         solver_iters = 50
-        point = TrajectoryPoint(
-            robot_configuration=np.array([1.0, 2.0, 3.0], dtype=np.float64),
-        )
-        weights = TrajectoryPointWeights(
-            w_robot_configuration=np.array([1.0, 2.0, 3.0], dtype=np.float64),
-        )
-        weighted_trajectory_point = WeightedTrajectoryPoint(point, weights)
-        weighted_trajectory_points = [weighted_trajectory_point]
-        armature = np.array([1.0, 2.0, 3.0], dtype=np.float64)
-        ee_name = "end_effector"
-        p_target = np.array([0.0, 0.0, 0.0], dtype=np.float64)
-
+        qp_iters = 200
+        termination_tolerance = 1e-3
+        eps_abs = 1e-6
+        eps_rel = 0
+        callbacks = False
         params = OCPParamsCrocoBase(
             dt=dt,
             T=T,
             solver_iters=solver_iters,
-            WeightedTrajectoryPoints=weighted_trajectory_points,
-            armature=armature,
-            ee_name=ee_name,
+            qp_iters=qp_iters,
+            termination_tolerance= termination_tolerance,
+            eps_abs = eps_abs,
+            eps_rel = eps_rel,
+            callbacks = callbacks
         )
-
+        print("hello")
         self.assertEqual(params.dt, dt)
         self.assertEqual(params.T, T)
-        self.assertEqual(params.qp_iters, 200)
+        self.assertEqual(params.qp_iters, qp_iters)
         self.assertEqual(params.solver_iters, solver_iters)
-        self.assertEqual(params.termination_tolerance, 1e-3)
-        self.assertEqual(params.eps_abs, 1e-6)
-        self.assertEqual(params.eps_rel, 0)
+        self.assertEqual(params.termination_tolerance, termination_tolerance)
+        self.assertEqual(params.eps_abs, eps_abs)
+        self.assertEqual(params.eps_rel, eps_rel)
         self.assertFalse(params.callbacks)
-        self.assertEqual(params.WeightedTrajectoryPoints, weighted_trajectory_points)
-        self.assertTrue(np.array_equal(params.armature, armature))
-        self.assertEqual(params.ee_name, ee_name)
 
 
 if __name__ == "__main__":
