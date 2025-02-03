51e932f766c67d0c00f7d4b9e843cd4c4e0b4657
Maximilien Naveau <maximilien.naveau@gmail.com>
2025-02-03 16:49:13 +0100

    Fix the unit-tests from the fix in loading the capsules properly.

--- agimus_controller/tests/test_robot_models.py
+++ agimus_controller/tests/test_robot_models.py
@@ -1,6 +1,6 @@
 import coal
 from copy import deepcopy
-from os import environ
+from os import environ, pathsep
 import unittest
 from pathlib import Path
 import example_robot_data as robex
@@ -28,7 +28,8 @@ except (ModuleNotFoundError, ImportError) as e:
     AMENT_AVAILABLE = False
 if AMENT_AVAILABLE:
     try:
-        get_package_share_directory("franka_description")
+        FRANKA_DESCRIPTION_PATH = get_package_share_directory("franka_description")
+        environ["AMENT_PREFIX_PATH"] += pathsep + FRANKA_DESCRIPTION_PATH
         FRANKA_DESCRIPTION_AVAILABLE = True
     except (OSError, PackageNotFoundError) as e:
         print(f"Error: {e}")  # Print the exact error
@@ -112,7 +113,7 @@ class TestRobotModelParameters(unittest.TestCase):
             RobotModelParameters(**self.valid_args)
 
 
-class TestRobotModels(unittest.TestCase):
+class TestRobotModelsAgainstExampleRobotData(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """
@@ -150,10 +151,20 @@ class TestRobotModels(unittest.TestCase):
     def test_no_meshes(self):
         self.params.urdf_meshes_dir = None
         # Clear paths where pinocchio searches for meshes
+        if "ROS_PACKAGE_PATH" in environ:
+            previous_ros_package_path = environ["ROS_PACKAGE_PATH"]
+        else:
+            previous_ros_package_path = ""
+        if "AMENT_PREFIX_PATH" in environ:
+            previous_ament_prefix_path = environ["AMENT_PREFIX_PATH"]
+        else:
+            previous_ament_prefix_path = ""
         environ["ROS_PACKAGE_PATH"] = ""
         environ["AMENT_PREFIX_PATH"] = ""
         with self.assertRaises(ValueError):
             RobotModels(self.params)
+        environ["ROS_PACKAGE_PATH"] = previous_ros_package_path
+        environ["AMENT_PREFIX_PATH"] = previous_ament_prefix_path
 
     def test_load_urdf_from_string(self):
         params = deepcopy(self.params)
@@ -184,6 +195,164 @@ class TestRobotModels(unittest.TestCase):
         np.testing.assert_array_equal(self.robot_models._q0, self.params.q0)
 
     def test_load_models_populates_models(self):
+        self.assertIsNotNone(self.robot_models.full_robot_model)
+        self.assertIsNotNone(self.robot_models.visual_model)
+        self.assertIsNotNone(self.robot_models.collision_model)
+
+    def test_reduced_robot_model(self):
+        self.assertTrue(
+            self.robot_models.robot_model.nq
+            == self.robot_models.full_robot_model.nq
+            - len(self.params.locked_joint_names)
+        )
+
+    def test_invalid_joint_name_raises_value_error(self):
+        # Modify a fresh instance of parameters for this test
+        self.params.locked_joint_names = ["InvalidJoint"]
+        with self.assertRaises(ValueError):
+            self.robot_models._apply_locked_joints()
+
+    def test_armature_property(self):
+        np.testing.assert_array_equal(self.robot_models.armature, self.params.armature)
+
+    def test_collision_pairs(self):
+        """Checking that the collision model has collision pairs."""
+        self.params.collision_as_capsule = False
+        self.robot_models.load_models()
+        self.assertEqual(
+            len(self.robot_models.collision_model.collisionPairs), 44
+        )  # Number of collision pairs in the panda model
+
+    def test_rnea(self):
+        """Checking that the RNEA method works."""
+        q = np.zeros(self.robot_models.robot_model.nq)
+        v = np.zeros(self.robot_models.robot_model.nv)
+        a = np.zeros(self.robot_models.robot_model.nv)
+        robot_data = self.robot_models.robot_model.createData()
+        pin.rnea(self.robot_models.robot_model, robot_data, q, v, a)
+
+    def test_franka_description_collision_models(self):
+        geom_obj_names_test = [
+            "panda_leftfinger_0",
+            "panda_leftfinger_1",
+            "panda_leftfinger_2",
+            "panda_leftfinger_3",
+            "panda_rightfinger_0",
+            "panda_rightfinger_1",
+            "panda_rightfinger_2",
+            "panda_rightfinger_3",
+        ]
+        geom_obj_names = [
+            geom_obj.name
+            for geom_obj in self.robot_models.collision_model.geometryObjects
+        ]
+        for geom_obj_name, geom_obj_name_test in zip(
+            geom_obj_names, geom_obj_names_test
+        ):
+            self.assertEqual(geom_obj_name, geom_obj_name_test)
+
+        geom_obj_types_test = [
+            coal.Box,
+            coal.Box,
+            coal.Box,
+            coal.Box,
+            coal.Box,
+            coal.Box,
+            coal.Box,
+            coal.Box,
+        ]
+        geom_obj_types = [
+            type(geom_obj.geometry)
+            for geom_obj in self.robot_models.collision_model.geometryObjects
+        ]
+        self.assertEqual(geom_obj_types, geom_obj_types_test)
+
+
+@unittest.skipIf(
+    (not XACRO_AVAILABLE or not AMENT_AVAILABLE or not FRANKA_DESCRIPTION_AVAILABLE),
+    f"Some dependencies amongst xacro (available ? {XACRO_AVAILABLE}) / "
+    f"franka_description (available ? {FRANKA_DESCRIPTION_AVAILABLE}) / "
+    f"ament_index_python (available ? {AMENT_AVAILABLE}) are not available",
+)
+class TestRobotModelsAgainstFrankaDescription(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        This method sets up the shared environment for all test cases in the class.
+        """
+        # Load the example robot model using example robot data to get the URDF path.
+        franka_description_path = Path(
+            get_package_share_directory("franka_description")
+        )
+        srdf_path = franka_description_path / "robots" / "fer" / "fer.srdf"
+        xacro_path = str(
+            franka_description_path / "robots" / "fer" / "fer.urdf.xacro",
+        )
+        params_path = str(
+            Path(__file__).parent / "resources" / "agimus_controller_params.yaml"
+        )
+        with open(params_path, "r") as file:
+            mpc_params = yaml.safe_load(file)["agimus_controller_node"][
+                "ros__parameters"
+            ]
+        urdf_xml = xacro.process_file(
+            xacro_path,
+            mappings={
+                "with_sc": "true",
+                "franka_controllers_params": "none",
+                "ros2_control": "false",
+            },
+        ).toxml()
+        # Hack for the moving joint name
+        model = pin.buildModelFromXML(urdf_xml)
+        locked_joint_names = [
+            jn
+            for jn in model.names
+            if jn not in ["universe"] + mpc_params["moving_joint_names"]
+        ]
+        cls.params = RobotModelParameters(
+            full_q0=np.zeros(model.nq),
+            q0=np.zeros(len(mpc_params["moving_joint_names"])),
+            free_flyer=False,
+            locked_joint_names=locked_joint_names,
+            urdf=urdf_xml,
+            srdf=srdf_path,
+            collision_as_capsule=True,
+            self_collision=True,
+            urdf_meshes_dir=franka_description_path,
+            armature=np.array(mpc_params["ocp"]["armature"]),
+        )
+
+    def setUp(self):
+        """
+        This method ensures that a fresh RobotModelParameters and RobotModels instance
+        are created for each test case.
+        """
+        self.params = deepcopy(self.params)
+        self.robot_models = RobotModels(self.params)
+
+    def test_no_meshes(self):
+        self.params.urdf_meshes_dir = None
+        # Clear paths where pinocchio searches for meshes
+        if "ROS_PACKAGE_PATH" in environ:
+            previous_ros_package_path = environ["ROS_PACKAGE_PATH"]
+        else:
+            previous_ros_package_path = ""
+        if "AMENT_PREFIX_PATH" in environ:
+            previous_ament_prefix_path = environ["AMENT_PREFIX_PATH"]
+        else:
+            previous_ament_prefix_path = ""
+        environ["ROS_PACKAGE_PATH"] = ""
+        environ["AMENT_PREFIX_PATH"] = ""
+        with self.assertRaises(ValueError):
+            RobotModels(self.params)
+        environ["ROS_PACKAGE_PATH"] = previous_ros_package_path
+        environ["AMENT_PREFIX_PATH"] = previous_ament_prefix_path
+
+    def test_initial_configuration(self):
+        np.testing.assert_array_equal(self.robot_models.q0, self.params.q0)
+
+    def test_load_models_populates_models(self):
         self.robot_models.load_models()
         self.assertIsNotNone(self.robot_models.full_robot_model)
         self.assertIsNotNone(self.robot_models.visual_model)
@@ -201,7 +370,7 @@ class TestRobotModels(unittest.TestCase):
     def test_collision_pairs(self):
         """Checking that the collision model has collision pairs."""
         self.assertEqual(
-            len(self.robot_models.collision_model.collisionPairs), 44
+            len(self.robot_models.collision_model.collisionPairs), 123
         )  # Number of collision pairs in the panda model
 
     def test_rnea(self):
@@ -212,56 +381,8 @@ class TestRobotModels(unittest.TestCase):
         robot_data = self.robot_models.robot_model.createData()
         pin.rnea(self.robot_models.robot_model, robot_data, q, v, a)
 
-    @unittest.skipIf(
-        (
-            not XACRO_AVAILABLE
-            or not AMENT_AVAILABLE
-            or not FRANKA_DESCRIPTION_AVAILABLE
-        ),
-        f"Some dependencies amongst xacro (available ? {XACRO_AVAILABLE}) / "
-        f"franka_description (available ? {FRANKA_DESCRIPTION_AVAILABLE}) / "
-        f"ament_index_python (available ? {AMENT_AVAILABLE}) are not available",
-    )
     def test_franka_description_collision_models(self):
-        franka_description_path = Path(
-            get_package_share_directory("franka_description")
-        )
-        srdf_path = franka_description_path / "robots" / "fer" / "fer.srdf"
-        xacro_path = str(
-            franka_description_path / "robots" / "fer" / "fer.urdf.xacro",
-        )
-        params_path = str(
-            Path(__file__).parent / "resources" / "agimus_controller_params.yaml"
-        )
-        with open(params_path, "r") as file:
-            mpc_params = yaml.safe_load(file)["agimus_controller_node"][
-                "ros__parameters"
-            ]
-        urdf_xml = xacro.process_file(
-            xacro_path,
-            mappings={"with_sc": "true"},
-        ).toxml()
-
-        # Hack for the moving joint name
-        model = pin.buildModelFromXML(urdf_xml)
-        locked_joint_names = [
-            jn
-            for jn in model.names
-            if jn not in ["universe"] + mpc_params["moving_joint_names"]
-        ]
-
-        self.params = RobotModelParameters(
-            full_q0=np.zeros(model.nq),
-            q0=np.zeros(len(mpc_params["moving_joint_names"])),
-            free_flyer=False,
-            locked_joint_names=locked_joint_names,
-            urdf=urdf_xml,
-            srdf=srdf_path,
-            collision_as_capsule=True,
-            self_collision=False,
-            armature=np.array(mpc_params["ocp"]["armature"]),
-        )
-        self.robot_models = RobotModels(self.params)
+        self.robot_models.load_models()
 
         geom_obj_names_test = [
             "fer_leftfinger_0",
@@ -289,7 +410,10 @@ class TestRobotModels(unittest.TestCase):
             geom_obj.name
             for geom_obj in self.robot_models.collision_model.geometryObjects
         ]
-        self.assertEqual(geom_obj_names, geom_obj_names_test)
+        for geom_obj_name, geom_obj_name_test in zip(
+            geom_obj_names, geom_obj_names_test
+        ):
+            self.assertEqual(geom_obj_name, geom_obj_name_test)
 
         geom_obj_types_test = [
             coal.Box,
@@ -317,7 +441,10 @@ class TestRobotModels(unittest.TestCase):
             type(geom_obj.geometry)
             for geom_obj in self.robot_models.collision_model.geometryObjects
         ]
-        self.assertEqual(geom_obj_types, geom_obj_types_test)
+        for geom_obj_type, geom_obj_type_test in zip(
+            geom_obj_types, geom_obj_types_test
+        ):
+            self.assertEqual(geom_obj_type, geom_obj_type_test)
 
 
 if __name__ == "__main__":
