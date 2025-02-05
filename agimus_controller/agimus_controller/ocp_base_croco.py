49f891d8c9f88beb2878422e58e35f5710b85303
Joseph Mirabel <josephmirabel@gmail.com>
2025-02-05 15:47:41 +0100

    [Minor] Access armature from RobotModels
    Co-authored-by: Naveau <maximilien.naveau@gmail.com>

--- agimus_controller/agimus_controller/ocp_base_croco.py
+++ agimus_controller/agimus_controller/ocp_base_croco.py
@@ -27,7 +27,7 @@ class OCPBaseCroco(OCPBase):
         # Setting the robot model
         self._robot_models = robot_models
         self._collision_model = self._robot_models.collision_model
-        self._armature = self._robot_models.params.armature
+        self._armature = self._robot_models.armature
 
         # Stat and actuation model
         self._state = crocoddyl.StateMultibody(self._robot_models.robot_model)
